5bdd79b4b1ea9987395c67ac5f1406533677004d
Carlos <Carlosj.pacheco15@gmail.com>
2021-11-24 20:40:44 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("Red")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
