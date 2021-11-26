c086c2346485e3f93737f66d7b7861755da68396
Carlos J Pacheco <Carlosj.pacheco15@gmail.com>
2021-11-26 17:44:26 -0400

    Day-18 finish Exercise

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-rgb = ["Red", "aquamarine1", "blue", "black", "green", "yellow", "maroon1", "tan2", "SlateBlue1", "DarkGoldenrod1",
-       "light salmon", "LemonChiffon1", "khaki1", "OrangeRed", "seashell1", "wheat2"]
-tim = Turtle()
-tim.shape("turtle")
+# rgb = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat",
+#        "SlateGray", "SeaGreen"]
+# TODO: Random Color
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color_random = (r, g, b)
+    return color_random
+
+
+tim = t.Turtle()
+# tim.shape("turtle")
 tim.color("Red")
 # timmy_the_turtle.forward(100)
 # timmy_the_turtle.left(90)
@@ -34,9 +46,32 @@ tim.color("Red")
 #     times -= 1
 
 # TODO: Random walk, different colors, the thickness of the line grow and the draw speed up
+# angle = [45, 90, 135, 180, 225, 270, 315, 360]
+# turn = ["right", "left"]
+# pen_speed = 1
+# tim.speed(1)
+# tim.width(5)
+# draw = 500
+# while draw >= 0:
+#     if pen_speed == 50 and tim.speed() < 10:
+#         tim.speed(tim.speed() + 1)
+#         pen_speed = 1
+#     if random.choice(turn) == "right":
+#         tim.right(random.randint(1, 360))
+#     else:
+#         tim.left(random.choice(angle))
+#     tim.color(random_color())
+#     tim.forward(20)
+#     pen_speed += 1
+#     draw -= 1
+#     print(draw)
 
+# TODO: Draw a Circle
+tim.speed("fastest")
+for n in range(72):
+    tim.color(random_color())
+    tim.circle(100)
+    tim.right(5)
 
-
-
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
